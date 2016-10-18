30d83141a5b0401bc77a991fed559b0fb2f56d62
Dave Harms <daveharmswebdev@gmail.com>
2016-10-18 17:31:19 -0500

    created patient model and replaced knex select with bookshelf model.fetch() in get users and get patients

--- server/server.js
+++ server/server.js
@@ -2,28 +2,71 @@
 
 const express = require('express')
 const app = express()
-const config = require('../knexfile').development
-const knex = require('knex')(config)
 const bodyParser = require('body-parser')
+
+const User = require('../model/user')
+const Patient = require('../model/patient')
+
+
+const { hash } = require('bcrypt')
 
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.get('/api/users', (req, res) => {
-	knex('User')
-		.select()
-		.then( data => res.send(data))
-})
 
-app.post('/api/users', (req, res) => {
-	knex('User')
-		.insert(req.body)
-		.then( data => {
-			console.log('data', data)
-			res.send(data)
+app.get('/api/users', (req, res) => {
+	new User().fetchAll()
+		.then( users => {
+			res.send(users)
 		})
 		.catch(console.error)
+})
+
+app.get('/api/users/:id', (req, res) => {
+	new User({id: req.params.id})
+		.fetch()
+		.then( user => res.send(user))
+		.catch(console.error)
+})
+
+app.post('/api/users', ({body}, res) => {
+	if (body.password === body.confirmation) {
+		knex('User')
+			.select()
+			.where('email', body.email)
+			.then(user => {
+				if (user.length) {
+					res.send(user)
+				} else {
+					hash(body.password, 15, (err, hash) => {
+						if (err) {
+							console.log(err)
+						} else {
+							const user = {
+								email: body.email,
+								password: hash
+							}
+							knex('User')
+								.insert(user)
+								.then( data => {
+									console.log('data', data)
+									res.send(data)
+								})
+								.catch(console.error)
+						}
+					})
+				}
+			}) 
+	} else {
+		res.send('password and confirmation did not match, you did not register')
+	}
+
+
+
+
+
+
 })
 
 app.get('/api/patients', (req, res) => {
