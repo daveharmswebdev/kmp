bc104cfd3260df65dc10b5291b3aafc7e5195e05
Dave Harms <daveharmswebdev@gmail.com>
2016-10-18 21:22:48 -0500

    installed passport, express-session, connect redis and set up redis in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 'use strict'
 
-const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
 const routes = require('../routes/index')
 
 const PORT = process.env.PORT || 3000
+
+const app = express()
+
+// middlewares
+app.use(session({
+	store: new RedisStore({
+		url: process.env.REDIS_URL || 'redis://localhost:6379'
+	}),
+	secret: 'daveisnumberone'
+}))
 
 app.use(bodyParser.urlencoded({extended:false}))
 
